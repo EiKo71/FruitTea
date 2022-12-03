92cb5d939678ee75998810cf0c63953f90dfedbb
EiKo71 <2079529583@qq.com>
2022-12-03 09:50:45 +0800

    第一次课设后端页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:7000'
+Vue.prototype.$http = axios
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 new Vue({
   router,
   render: h => h(App)
